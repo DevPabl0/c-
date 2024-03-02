9e6cc32a4bad7aa33a06086cc20c91dc61141813
DevPabl0 <pablomarques1513@gmail.com>
2024-03-01 22:04:29 -0300

    Icons

--- ProjetoLojaABC/frmMenuPrincipal.Designer.cs
+++ ProjetoLojaABC/frmMenuPrincipal.Designer.cs
@@ -29,79 +29,105 @@ namespace ProjetoLojaABC
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnClientes = new System.Windows.Forms.Button();
-            this.btnFumcionarios = new System.Windows.Forms.Button();
             this.btnProdutos = new System.Windows.Forms.Button();
-            this.btnVendas = new System.Windows.Forms.Button();
-            this.btnFornecedores = new System.Windows.Forms.Button();
-            this.btnUsuarios = new System.Windows.Forms.Button();
             this.btnVoltar = new System.Windows.Forms.Button();
+            this.btnUsuarios = new System.Windows.Forms.Button();
+            this.btnFornecedores = new System.Windows.Forms.Button();
+            this.btnVendas = new System.Windows.Forms.Button();
+            this.btnFumcionarios = new System.Windows.Forms.Button();
+            this.btnClientes = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // btnClientes
-            // 
-            this.btnClientes.Location = new System.Drawing.Point(33, 12);
-            this.btnClientes.Name = "btnClientes";
-            this.btnClientes.Size = new System.Drawing.Size(172, 226);
-            this.btnClientes.TabIndex = 0;
-            this.btnClientes.Text = "Clientes";
-            this.btnClientes.UseVisualStyleBackColor = true;
-            this.btnClientes.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // btnFumcionarios
-            // 
-            this.btnFumcionarios.Location = new System.Drawing.Point(33, 260);
-            this.btnFumcionarios.Name = "btnFumcionarios";
-            this.btnFumcionarios.Size = new System.Drawing.Size(172, 226);
-            this.btnFumcionarios.TabIndex = 1;
-            this.btnFumcionarios.Text = "Funcionários";
-            this.btnFumcionarios.UseVisualStyleBackColor = true;
             // 
             // btnProdutos
             // 
+            this.btnProdutos.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProdutos.ForeColor = System.Drawing.Color.AliceBlue;
+            this.btnProdutos.Image = global::ProjetoLojaABC.Properties.Resources.commerce_online_buy_store_business_box_product_item_icon_219867__1_;
             this.btnProdutos.Location = new System.Drawing.Point(259, 12);
             this.btnProdutos.Name = "btnProdutos";
             this.btnProdutos.Size = new System.Drawing.Size(172, 226);
             this.btnProdutos.TabIndex = 2;
             this.btnProdutos.Text = "Produtos";
+            this.btnProdutos.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnProdutos.UseVisualStyleBackColor = true;
-            // 
-            // btnVendas
-            // 
-            this.btnVendas.Location = new System.Drawing.Point(259, 260);
-            this.btnVendas.Name = "btnVendas";
-            this.btnVendas.Size = new System.Drawing.Size(172, 226);
-            this.btnVendas.TabIndex = 3;
-            this.btnVendas.Text = "Vendas";
-            this.btnVendas.UseVisualStyleBackColor = true;
-            // 
-            // btnFornecedores
-            // 
-            this.btnFornecedores.Location = new System.Drawing.Point(484, 12);
-            this.btnFornecedores.Name = "btnFornecedores";
-            this.btnFornecedores.Size = new System.Drawing.Size(172, 226);
-            this.btnFornecedores.TabIndex = 4;
-            this.btnFornecedores.Text = "Fornecedores";
-            this.btnFornecedores.UseVisualStyleBackColor = true;
-            this.btnFornecedores.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // btnUsuarios
-            // 
-            this.btnUsuarios.Location = new System.Drawing.Point(484, 260);
-            this.btnUsuarios.Name = "btnUsuarios";
-            this.btnUsuarios.Size = new System.Drawing.Size(172, 226);
-            this.btnUsuarios.TabIndex = 5;
-            this.btnUsuarios.Text = "Usuários";
-            this.btnUsuarios.UseVisualStyleBackColor = true;
             // 
             // btnVoltar
             // 
+            this.btnVoltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVoltar.ForeColor = System.Drawing.Color.DarkGreen;
             this.btnVoltar.Location = new System.Drawing.Point(596, 504);
             this.btnVoltar.Name = "btnVoltar";
             this.btnVoltar.Size = new System.Drawing.Size(157, 45);
             this.btnVoltar.TabIndex = 6;
             this.btnVoltar.Text = "Voltar";
             this.btnVoltar.UseVisualStyleBackColor = true;
+            // 
+            // btnUsuarios
+            // 
+            this.btnUsuarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUsuarios.ForeColor = System.Drawing.Color.AliceBlue;
+            this.btnUsuarios.Image = global::ProjetoLojaABC.Properties.Resources.group__1_;
+            this.btnUsuarios.Location = new System.Drawing.Point(484, 260);
+            this.btnUsuarios.Name = "btnUsuarios";
+            this.btnUsuarios.Size = new System.Drawing.Size(172, 226);
+            this.btnUsuarios.TabIndex = 5;
+            this.btnUsuarios.Text = "Usuários";
+            this.btnUsuarios.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnUsuarios.UseVisualStyleBackColor = true;
+            // 
+            // btnFornecedores
+            // 
+            this.btnFornecedores.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFornecedores.ForeColor = System.Drawing.Color.AliceBlue;
+            this.btnFornecedores.Image = global::ProjetoLojaABC.Properties.Resources.wholesale;
+            this.btnFornecedores.Location = new System.Drawing.Point(484, 12);
+            this.btnFornecedores.Name = "btnFornecedores";
+            this.btnFornecedores.Size = new System.Drawing.Size(172, 226);
+            this.btnFornecedores.TabIndex = 4;
+            this.btnFornecedores.Text = "Fornecedores";
+            this.btnFornecedores.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnFornecedores.UseVisualStyleBackColor = true;
+            this.btnFornecedores.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btnVendas
+            // 
+            this.btnVendas.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVendas.ForeColor = System.Drawing.Color.AliceBlue;
+            this.btnVendas.Image = global::ProjetoLojaABC.Properties.Resources.coupon;
+            this.btnVendas.Location = new System.Drawing.Point(259, 260);
+            this.btnVendas.Name = "btnVendas";
+            this.btnVendas.Size = new System.Drawing.Size(172, 226);
+            this.btnVendas.TabIndex = 3;
+            this.btnVendas.Text = "Vendas";
+            this.btnVendas.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnVendas.UseVisualStyleBackColor = true;
+            // 
+            // btnFumcionarios
+            // 
+            this.btnFumcionarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFumcionarios.ForeColor = System.Drawing.Color.AliceBlue;
+            this.btnFumcionarios.Image = global::ProjetoLojaABC.Properties.Resources.group;
+            this.btnFumcionarios.Location = new System.Drawing.Point(33, 260);
+            this.btnFumcionarios.Name = "btnFumcionarios";
+            this.btnFumcionarios.Size = new System.Drawing.Size(172, 226);
+            this.btnFumcionarios.TabIndex = 1;
+            this.btnFumcionarios.Text = "Funcionários";
+            this.btnFumcionarios.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnFumcionarios.UseVisualStyleBackColor = true;
+            // 
+            // btnClientes
+            // 
+            this.btnClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClientes.ForeColor = System.Drawing.Color.AliceBlue;
+            this.btnClientes.Image = global::ProjetoLojaABC.Properties.Resources.client;
+            this.btnClientes.Location = new System.Drawing.Point(33, 12);
+            this.btnClientes.Name = "btnClientes";
+            this.btnClientes.Size = new System.Drawing.Size(172, 226);
+            this.btnClientes.TabIndex = 0;
+            this.btnClientes.Text = "Clientes";
+            this.btnClientes.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnClientes.UseVisualStyleBackColor = true;
+            this.btnClientes.Click += new System.EventHandler(this.button1_Click);
             // 
             // frmMenuPrincipal
             // 
@@ -122,13 +148,12 @@ namespace ProjetoLojaABC
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnClientes;
         private System.Windows.Forms.Button btnFumcionarios;
         private System.Windows.Forms.Button btnProdutos;
         private System.Windows.Forms.Button btnVendas;
         private System.Windows.Forms.Button btnFornecedores;
         private System.Windows.Forms.Button btnUsuarios;
         private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Button btnClientes;
     }
 }
